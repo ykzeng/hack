d16341943349537b802a6bde0c552123f6189edd
Vincent Tsang <yukun.zeng.hit@outlook.com>
2016-11-02 00:29:34 -0500

    a more elegant version of strange counter

--- java/src/com/yzeng/hack/easy/StrangeCounter.java
+++ java/src/com/yzeng/hack/easy/StrangeCounter.java
@@ -6,17 +6,18 @@ public class StrangeCounter {
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         long t = in.nextLong();
-        
-        long cycle = 3, curretT = 0;
-        while (true) {
-			for (long i = cycle; i > 0; i--) {
-				curretT ++;
-				if (curretT == t) {
-					System.out.println(i);
-					return;
-				}
-			}
-			cycle = cycle * 2;
+        if (t <= 3) {
+			System.out.println(3 - t + 1);
+			return;
 		}
+        
+        long currentMax = 3, lastMax = 0, currentCycle = 0;
+        int  i = 2;
+        for (i = 2; t > currentMax; i++) {
+        	lastMax = currentMax;
+			currentMax = 3 * (long)(1 - Math.pow((double)2, (double)i)) / (1 - 2);
+			currentCycle = (3 * ((long)Math.pow(2, (i-1))));
+		}
+        System.out.println((currentCycle - (t - lastMax) + 1));
     }
 }
