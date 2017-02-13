c2497e6a47b4290569b35d52c6157ec260fce6f3
Vincent Tsang <yukun.zeng.hit@outlook.com>
2017-02-12 22:03:52 -0600

    added minimum bst and reverse 2nd half linked list

---
+++ java/src/com/yzeng/amazon/MinBST.java
@@ -0,0 +1,32 @@
+package com.yzeng.amazon;
+
+import com.yzeng.datastructures.TreeNode;
+
+public class MinBST {
+	public static void main(String[] args){
+		
+	}
+	
+	public static int minBST(TreeNode tNode){
+//		if (tNode.left != null && tNode.right != null) {
+//			int leftTotal = minBST(tNode.left), rightTotal = minBST(tNode.right);
+//			return ((leftTotal > rightTotal ? rightTotal : leftTotal) + tNode.val);
+//		}
+//		else if (tNode.left == null && tNode.right == null) {
+//			return tNode.val;
+//		}
+//		else {
+//			return minBST(tNode.left == null ? tNode.right : tNode.left);
+//		}
+		if (tNode == null) 
+			return 0;
+		else if (tNode.left == null && tNode.right == null) 
+			return tNode.val;
+		else if (tNode.left != null && tNode.right != null){
+			int leftTotal = minBST(tNode.left), rightTotal = minBST(tNode.right);
+			return ((leftTotal > rightTotal ? rightTotal : leftTotal) + tNode.val);
+		}
+		else
+			return minBST(tNode.left == null ? tNode.right : tNode.left) + tNode.val;
+	}
+}
---
+++ java/src/com/yzeng/amazon/Reverse2ndHalfList.java
@@ -0,0 +1,36 @@
+package com.yzeng.amazon;
+
+import com.yzeng.datastructures.ListNode;
+
+public class Reverse2ndHalfList {
+	public static void main(String[] args){
+		ListNode node = new ListNode(1), p = node;
+		for (int i = 2; i < 10; i++) {
+			node.next = new ListNode(i);
+			node = node.next;
+		}
+		p.printAll();
+		node = reverse2ndHalfList(p);
+		node.printAll();
+	}
+	
+	public static ListNode reverse2ndHalfList(ListNode head){
+		if (head == null || head.next == null) {
+			return head;
+		}
+		ListNode fastP = head, slowP = head;
+		while (fastP.next != null && fastP.next.next != null) {
+			fastP = fastP.next.next;
+			slowP = slowP.next;
+		}
+		ListNode cur = slowP.next, newList = null, p;
+		while (cur != null) {
+			p = cur;
+			cur = cur.next;
+			p.next = newList;
+			newList = p;
+		}
+		slowP.next = newList;
+		return head;
+	}
+}
