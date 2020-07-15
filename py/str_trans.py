1d368eaddb11804de2b24bb5dc982f4eeb971b19
Yukun Zeng <yukun.zeng.hit@outlook.com>
2020-07-15 18:32:59 -0500

    git ignore for vscode, tencent interview question for data science

---
+++ py/str_trans.py
@@ -0,0 +1,17 @@
+#s = "Cloud based"
+s = "Predictive multi-objective network optimization for wireless roaming with ML forecasting (patent pending)"
+# t = s.split(' ')
+# lst = []
+# for i in range(0, len(t)):
+#     w = t[i]
+#     r = ''
+#     for j in range(len(w)-1, -1, -1):
+#         r += w[j]
+#     lst.append(r)
+# print(lst)
+# res = ' '.join(lst)
+# print(res)
+# Tencent interview question for Data Scientist
+# given a string with words separated by spaces
+# reverse every word without changing the sequences of the sentence
+# TODO use stack to load every word and pop them out whenever a space is encountered
